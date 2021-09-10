246030622c397f2eb396e9adf2eba3fda827b1d5
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 11:14:32 -0700

    check if connection error

--- public/dbOffline.js
+++ public/dbOffline.js
@@ -2,16 +2,23 @@ let db;
 
 const request = indexedDB.open("budget", 1);
 
+// create new object store
 request.onupgradeneeded = function (event) {
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
 
+// check if online
 request.onsuccess = function (event) {
   db = event.target.result;
 
   if (navigator.onLine) {
     // make a checkdb() function;
   }
+};
+
+// check if there is a connection error
+request.onerror = function (event) {
+  console.log("Error: Cannot connected to database " + event.target.errorCode);
 };
 
