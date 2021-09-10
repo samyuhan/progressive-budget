442594e1bcc0f81e536021a2bcf731a38e3bf354
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 11:12:03 -0700

    create new object store for pending

--- public/dbOffline.js
+++ public/dbOffline.js
@@ -2,3 +2,7 @@ let db;
 
 const request = indexedDB.open("budget", 1);
 
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
