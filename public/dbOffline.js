8b48e3723874c68f781276de88b94cb022c4d363
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-10 11:13:07 -0700

    check if online

--- public/dbOffline.js
+++ public/dbOffline.js
@@ -6,3 +6,12 @@ request.onupgradeneeded = function (event) {
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    // make a checkdb() function;
+  }
+};
+
